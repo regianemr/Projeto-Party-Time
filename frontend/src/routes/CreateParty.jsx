afef8096c63b763f117c4a9c129fbdafe2867a54
Regiane <regianemelo1995@gmail.com>
2024-10-01 11:48:52 -0300

    finalizando criacao de festas

--- frontend/src/routes/CreateParty.jsx
+++ frontend/src/routes/CreateParty.jsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react'
 
 import { useNavigate } from "react-router-dom"
 
+import  useToast  from '../hook/useToast'
+
 import './Form.css'
 
 const CreateParty = () => {
@@ -15,6 +17,8 @@ const CreateParty = () => {
   const [budget, setBudget] = useState(0)
   const [image, setImage] = useState("")
   const [partyServices, setPartyServices] = useState([])
+
+  const navigate = useNavigate()
 
   // Load services
   useEffect(() => {
@@ -45,22 +49,30 @@ const CreateParty = () => {
    console.log(partyServices)
   }
 
-
   // Create a new party
-  const createParty = (e) => {
+  const createParty = async (e) => {
     e.preventDefault();
 
-    const party = {
-      title,
-      author, 
-      description,
-      budget,
-      image,
-      services: partyServices,
+    try {
+      const party = {
+        title,
+        author, 
+        description,
+        budget,
+        image,
+        services: partyServices,
+      }
+  
+      const res = await partyFetch.post("/parties", party)
+  
+      if(res.status === 201) {
+        navigate("/")
+  
+        useToast(res.data.msg)
+      }
+    } catch (error) {
+      useToast(error.response.data.msg, "error")
     }
-
-    console.log(party)
-
   }
 
   return <div className="form-page">
